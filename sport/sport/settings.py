8c50e414ceb2d9a701cf12a68d3ca6b69fedf928
asalas <asalas@mybox.(none)>
2012-11-20 19:55:16 +0100

    Ignoring3

--- sport/sport/settings.py
+++ sport/sport/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'asalas_bd',                      # Or path to database file if using sqlite3.
         'USER': 'asalas',                      # Not used with sqlite3.
-        'PASSWORD': 'gggggggggggggg',                  # Not used with sqlite3.
+        'PASSWORD': 'hhhhhhhhhhhh',                  # Not used with sqlite3.
         'HOST': 'mysql2.alwaysdata.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
