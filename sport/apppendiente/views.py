bfe0516d2533a4f40128b6b1a5780620d1c5c0d0
asalas <asalas@mybox.(none)>
2012-11-24 12:43:53 +0100

    Delete: ya no necesito el frame inferior izquierdo

--- sport/apppendiente/views.py
+++ sport/apppendiente/views.py
@@ -5,4 +5,5 @@ from models import *
 
 def count(request):
     total_pendientes = Pendiente.objects.count()
-    return render_to_response('count.html',{'total_pendientes':total_pendientes},context_instance=RequestContext(request))
+    lista_pend = Pendiente.objects.all()
+    return render_to_response('count.html',{'total_pendientes':total_pendientes,'lista_pend':lista_pend},context_instance=RequestContext(request))
