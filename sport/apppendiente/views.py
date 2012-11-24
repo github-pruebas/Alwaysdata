72311c4158f5e3d6e1450189cec0d4841ea2c8d6
asalas <asalas@mybox.(none)>
2012-11-24 11:46:57 +0100

    Subidas y correcciones varias

--- sport/apppendiente/views.py
+++ sport/apppendiente/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+# -*- encoding: utf-8 -*-
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+from models import *
+
+def count(request):
+    total_pendientes = Pendiente.objects.count()
+    return render_to_response('count.html',{'total_pendientes':total_pendientes},context_instance=RequestContext(request))
--- sport/sport/urls.py
+++ sport/sport/urls.py
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^sport/', include('sport.appsport.urls')),
+    url(r'^count/', 'apppendiente.views.count'),
 )
