ff98a20ddf6daa6537926110edd105d4130e3509
asalas <asalas@hotmail.com>
2014-03-22 23:17:28 +0100

    Sphinx 1

--- sport/appsport/views.py
+++ sport/appsport/views.py
@@ -4,5 +4,13 @@ from django.shortcuts import redirect, render_to_response, get_object_or_404
 from models import *
 
 def home_page(request):
+   """short description of the function home_page
+
+    longish explanation: redirects to "/inicio"
+
+    :param: request
+
+    :returns: home page
+    """
     #return redirect('http://www.as.com/')
     return redirect('/inicio')
